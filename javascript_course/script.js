478ee6c9b66103f45ea00a0053c78d5246424664
sushant kulkarni <83342285+sushantsk1@users.noreply.github.com>
2023-07-30 13:15:57 +0530

    Add files via upload

---
+++ javascript_course/script.js
@@ -0,0 +1,14 @@
+// data types
+var str = "sushant kulkarni";
+var roll = 40;
+var marks = {
+  ravi: 49,
+  shubham: 99,
+};
+document.write(str);
+document.write("roll number is " + roll);
+document.write("marks are " + marks);
+
+var a = true;
+var a = false;
+console.log(a);
